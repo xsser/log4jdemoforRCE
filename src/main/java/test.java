cf84296fef541efab6b6eff02125776034fd0eeb
infosec_op <infosec_op@cn1.global.ctrip.com>
2021-12-13 14:28:12 +0800

    增加原生jndi注入的demo在一起 方便对比学习

---
+++ src/main/java/test.java
@@ -0,0 +1,12 @@
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+public class test {
+    public static void main(String[] args) throws NamingException {
+        System.setProperty("java.rmi.server.useCodebaseOnly", "false");
+        System.setProperty("com.sun.jndi.rmi.object.trustURLCodebase", "true");
+        Context context = new  InitialContext();
+        context.lookup("rmi://127.0.0.1:1099/hvl9hr");
+    }
+}
